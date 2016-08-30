849affb1a2556095afd4334a9e32007390c2e890
Dan Lindsay <dlindsay72@hotmail.com>
2016-08-30 15:49:19 -0400

    Activity indicator added over button with constraints

--- Jot/ViewController.swift
+++ Jot/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Speech
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -15,10 +17,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
